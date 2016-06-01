4ab13bc54cfe7fb1cfd36cdf8aa6517b18a033fc
abnaki <abnaki>
2016-06-01 18:20:49 -0400

    OK

---
+++ Wpf/Starter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Abnaki.Albiruni
+{
+    class Starter
+    {
+        [STAThread]
+        public static int Main(string[] args)
+        {
+            return Abnaki.Windows.Software.Wpf.Ultimate.UltimateStarter<MainControl>.Start(args);
+        }
+    }
+}
