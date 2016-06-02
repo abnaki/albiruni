95bdde947a4f76384deb753e0743ac18f767b733
abnaki <abnaki>
2016-06-02 12:07:54 -0400

    Nothing but new GpxFile()

---
+++ Tests/ProviderTest1/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Abnaki.Albiruni;
+
+namespace ProviderTest1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestGpx()
+        {
+            var gfile = new Abnaki.Albiruni.Providers.GpxFile();
+
+        }
+    }
+}
