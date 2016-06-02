28e03834876a659ba0b08d239e4d96b4dfaf9e83
abnaki <abnaki>
2016-06-02 12:07:54 -0400

    1.0.0.0

---
+++ AlbiruniAssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Abnaki Way Components")]
+[assembly: AssemblyProduct("Albiruni")]
+[assembly: AssemblyCopyright("Copyright © 2016 Abnaki Way Components")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
