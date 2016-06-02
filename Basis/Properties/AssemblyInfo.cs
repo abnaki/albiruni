9daaeff770312f7d018efbfe175ea8979e6f559b
abnaki <abnaki>
2016-06-02 12:07:54 -0400

    Empty

---
+++ Basis/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Albiruni Basis")]
+[assembly: AssemblyDescription("")]
---
+++ Basis/Providers/GpxFile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Abnaki.Albiruni.Providers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class GpxFile
+    {
+
+    }
+}
