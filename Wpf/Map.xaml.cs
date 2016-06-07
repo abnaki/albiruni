c1e6fbde7cb5b51364caf3c76463e3f671c8deb1
abnaki <abnaki>
2016-06-06 20:44:39 -0400

    +Map.xaml, bare bones

---
+++ Wpf/Map.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using MapControl;
+
+namespace Abnaki.Albiruni
+{
+    /// <summary>
+    /// Interaction logic for Map.xaml
+    /// </summary>
+    public partial class Map : UserControl
+    {
+        public Map()
+        {
+            InitializeComponent();
+        }
+
+        #region Originally from xamlmapcontrol/SampleApps/WpfApplication/MainWindow.xaml.cs
+
+        private void MapMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                map.ZoomMap(e.GetPosition(map), Math.Floor(map.ZoomLevel + 1.5));
+                //map.TargetCenter = map.ViewportPointToLocation(e.GetPosition(map));
+            }
+        }
+
+        private void MapMouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                map.ZoomMap(e.GetPosition(map), Math.Ceiling(map.ZoomLevel - 1.5));
+            }
+        }
+
+        private void MapManipulationInertiaStarting(object sender, ManipulationInertiaStartingEventArgs e)
+        {
+            e.TranslationBehavior.DesiredDeceleration = 0.001;
+        }
+
+        private void MapItemTouchDown(object sender, TouchEventArgs e)
+        {
+            var mapItem = (MapItem)sender;
+            mapItem.IsSelected = !mapItem.IsSelected;
+            e.Handled = true;
+        }
+
+        #endregion
+
+    }
+}
