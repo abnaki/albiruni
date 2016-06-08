b9a62c5432ab55fdde1a61adc15d2684cd779b75
abnaki <abnaki>
2016-06-08 14:07:07 -0400

    Has Path and GpxFile

--- Basis/Tree/Source.cs
+++ Basis/Tree/Source.cs
@@ -1,13 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.IO;
+
+using Abnaki.Windows;
 
 namespace Abnaki.Albiruni.Tree
 {
     /// <summary>
     /// Base class; may point to a file
     /// </summary>
-    public class Source
+    public class Source : IComparable<Source>
     {
+        public Source()
+        {
+            
+        }
+
+        public Source(FileInfo fi)
+        {
+            this.GpxFile = new Providers.GpxFile();
+            this.GpxFile.Deserialize(fi);
+
+            DirectoryInfo dicur = new DirectoryInfo(Environment.CurrentDirectory); // may want to pass in
+
+            this.Path = AbnakiFile.RelativePath(fi, dicur);
+        }
+
+        public string Path { get; private set; }
+
+        public Providers.GpxFile GpxFile { get; private set; } // may want to generalize type
+
+        public override string ToString()
+        {
+            return Path;
+        }
+
+        public int CompareTo(Source other)
+        {
+            return this.Path.CompareTo(other.Path);
+        }
     }
 }
