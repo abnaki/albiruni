54161e3cf2d3769462d023568adfb3abac7c4963
abnaki <abnaki>
2016-06-18 11:32:33 -0400

    +Symbols, test of Diamond

--- Wpf/MapViewModel.cs
+++ Wpf/MapViewModel.cs
@@ -16,15 +16,17 @@ namespace Abnaki.Albiruni
         public MapViewModel()
         {
             Rectangles = new ObservableCollection<MapRectangle>();
+            Symbols = new ObservableCollection<MapPath>();
         }
 
         public Location MapCenter { get; set; }
 
         public ObservableCollection<MapRectangle> Rectangles { get; private set; }
+        public ObservableCollection<MapPath> Symbols { get; set; }
 
-        // Point and Polyline did not belong to MapControl library.
         //public ObservableCollection<Point> Points { get; set; }
         //public ObservableCollection<Polyline> Polylines { get; set; }
+
 
         public void Testing()
         {
@@ -37,6 +39,10 @@ namespace Abnaki.Albiruni
             r.East = r.West + 0.1;
             r.Fill = new SolidColorBrush(Color.FromArgb((byte)32, (byte)255, (byte)0, (byte)0));
             Rectangles.Add(r);
+
+            var diamond = new Abnaki.Albiruni.Graphic.Symbol.Diamond(MapCenter, 0.01, 0.015);
+            diamond.Fill = new SolidColorBrush(Color.FromArgb((byte)64, (byte)0, (byte)0, (byte)255));
+            this.Symbols.Add(diamond);
         }
 
     }
