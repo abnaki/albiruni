7e38966df22109a269feb521a68ca1ad218bd850
abnaki <abnaki>
2016-06-18 11:32:32 -0400

    Good

---
+++ Wpf/Graphic/Symbol/Diamond.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using MapControl;
+
+namespace Abnaki.Albiruni.Graphic.Symbol
+{
+    class Diamond : MapControl.MapPolyline
+    {
+        public Diamond(Location center, double width, double height)
+        {
+            this.Locations = new Location[]{
+                new Location(center.Latitude + height / 2, center.Longitude),
+                new Location(center.Latitude, center.Longitude - width/2), 
+                new Location(center.Latitude - height/2, center.Longitude),
+                new Location(center.Latitude, center.Longitude + width/2)
+            };
+
+            IsClosed = true;
+        }
+    }
+}
