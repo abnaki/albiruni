928b1104e00a28ae377aecd62aa23bb0bb2a4c98
abnaki <abnaki>
2016-06-18 11:55:14 -0400

    +Track

---
+++ Wpf/Graphic/Curve/Track.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Abnaki.Albiruni.Graphic.Curve
+{
+    class Track : MapControl.MapPolyline
+    {
+        public Track()
+        {
+            // may want to set Locations and Stroke
+        }
+    }
+}
