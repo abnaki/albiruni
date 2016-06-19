c46511719e73eb2036592bd283b6e394786ef5e3
abnaki <abnaki>
2016-06-19 16:28:24 -0400

    ViewportChanged handler

--- Wpf/Map.xaml.cs
+++ Wpf/Map.xaml.cs
@@ -1,17 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
+using System.Windows.Input;
 using System.Windows.Shapes;
+using System.Diagnostics;
 
 using MapControl;
 
@@ -20,20 +16,27 @@ namespace Abnaki.Albiruni
     /// <summary>
     /// Interaction logic for Map.xaml
     /// </summary>
-    public partial class Map : UserControl
+    partial class Map : UserControl
     {
         public Map()
         {
             InitializeComponent();
+
+            vptimer.Tick += vptimer_Tick;
+        }
+
+        public new MapViewModel DataContext
+        {
+            get { return base.DataContext as MapViewModel; }
+            set { base.DataContext = value; }
         }
 
         protected override void OnInitialized(EventArgs e)
         {
             base.OnInitialized(e);
 
-            MapViewModel vm = new MapViewModel();
-            vm.Testing();
-            this.DataContext = vm;
+            this.DataContext = new MapViewModel();
+            this.DataContext.Testing();
         }
 
         #region Originally from xamlmapcontrol/SampleApps/WpfApplication/MainWindow.xaml.cs
@@ -66,6 +69,37 @@ namespace Abnaki.Albiruni
             mapItem.IsSelected = !mapItem.IsSelected;
             e.Handled = true;
         }
+
+        #endregion
+
+        #region Community-suggested xamlmapcontrol viewport change event using a timer
+
+        void vptimer_Tick(object sender, EventArgs e)
+        {
+            vptimer.Stop();
+
+            Point pNorthWest = new Point(0, 0);
+            Point pSouthEast = new Point(map.ActualWidth, map.ActualHeight);
+            Location locNorthWest = map.ViewportPointToLocation(pNorthWest);
+            Location locSouthEast = map.ViewportPointToLocation(pSouthEast);
+
+            this.DataContext.ViewPortRect = new MapRectangle()
+            {
+                North = locNorthWest.Latitude,
+                South = locSouthEast.Latitude,
+                West = locNorthWest.Longitude,
+                East = locSouthEast.Longitude
+            };
+        }
+
+        System.Windows.Threading.DispatcherTimer vptimer 
+            = new System.Windows.Threading.DispatcherTimer() { Interval = TimeSpan.FromMilliseconds(200) };
+
+        private void map_ViewportChanged(object sender, EventArgs e)
+        {
+            vptimer.Stop(); vptimer.Start(); // MS idea of Reset
+        }
+
 
         #endregion
 
