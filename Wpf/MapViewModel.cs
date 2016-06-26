2cfa95ea5b846d7e9e10f051d12846f71a46cdff
abnaki <abnaki>
2016-06-26 17:16:42 -0400

    SetViewPort(), UpdateRectangles(), DescentResult logic...good

--- Wpf/MapViewModel.cs
+++ Wpf/MapViewModel.cs
@@ -7,6 +7,7 @@ using System.ComponentModel;
 using System.Windows.Media;
 
 using Abnaki.Albiruni.Tree;
+using Abnaki.Albiruni.Graphic;
 using Abnaki.Windows.Software.Wpf;
 using MapControl;
 using PropertyChanged;
@@ -22,13 +23,14 @@ namespace Abnaki.Albiruni
             Symbols = new ObservableCollection<MapPath>();
             Tracks = new ObservableCollection<MapPath>();
             ViewPortRect = new MapRectangle(); // unspecified
-            MinimumPrecision = 1;
+            MinimumPrecision = 0.2;
 
             MessageTube.Subscribe<Node>(HandleTree);
         }
 
         public Location MapCenter { get; set; }
 
+        // want to use BulkObservableCollection or similar
         public ObservableCollection<MapRectangle> Rectangles { get; private set; }
         public ObservableCollection<MapPath> Symbols { get; private set; }
         public ObservableCollection<MapPath> Tracks { get; private set; }
@@ -37,41 +39,97 @@ namespace Abnaki.Albiruni
 
         /// <summary>Last known rectangle; not bound to control.  i.e. set{} does not affect the map.
         /// </summary>
-        public MapRectangle ViewPortRect { get; set; }
+        MapRectangle ViewPortRect { get; set; }
 
-        //public ObservableCollection<Point> Points { get; set; }
-        //public ObservableCollection<Polyline> Polylines { get; set; }
+        public void SetViewPort(MapRectangle rect)
+        {
+            if (this.ViewPortRect.EqualCoordinates(rect))
+            {
+                // skip
+            }
+            else
+            {
+                this.ViewPortRect = rect;
+                UpdateRectangles(newRoot: false);
+            }
+        }
+
+        Node RootNode { get; set; }
+
+        System.Windows.Media.Brush m_defaultFillBrush = new SolidColorBrush(Color.FromArgb((byte)32, (byte)255, (byte)0, (byte)0));
+
 
         public void HandleTree(Node root)
         {
-            ClearAdornments();
-
             //root.DebugPrint();
 
-            Node.Statistic stat = root.GetStatistic();
+            bool newRoot = root != RootNode;
+            RootNode = root;
+
+            UpdateRectangles(newRoot);
+        }
+
+        /// <summary>
+        /// depends on RootNode and ViewPortRect
+        /// </summary>
+        void UpdateRectangles(bool newRoot)
+        {
+            Rectangles.Clear();
+
+            if (RootNode == null)
+                return; // OK
+
+            Node.Statistic stat = RootNode.GetStatistic();
             Debug.WriteLine(stat);
             if (stat.ContentSummary.Points > 0)
             {
-                double midLat = (stat.ContentSummary.MinLatitude.Value + stat.ContentSummary.MaxLatitude.Value) / 2;
-                double midLon = (stat.ContentSummary.MinLongitude.Value + stat.ContentSummary.MaxLongitude.Value) / 2;
+                if (newRoot)
+                {
+                    double midLat = (stat.ContentSummary.MinLatitude.Value + stat.ContentSummary.MaxLatitude.Value) / 2;
+                    double midLon = (stat.ContentSummary.MinLongitude.Value + stat.ContentSummary.MaxLongitude.Value) / 2;
+                    MapCenter = new Location(midLat, midLon);
+                    // note that the view may still remain zoomed in far enough to not intersect any points in the tree.  ergo, user must zoom out.
+                }
 
                 // predicting bounds from last known view
                 double viewWidth = ViewPortRect.East - ViewPortRect.West;
                 double viewHeight = ViewPortRect.North - ViewPortRect.South;
 
-                MapCenter = new Location(midLat, midLon);
+                // rather than MapRectangle, want an abstraction to allow for two disjoint shapes 
+                // such as two rectangles abutting longitude +- 180; 
+                // and easily check for its intersection with a Node.
 
-                MapRectangle logicalBound = new MapRectangle()
+                MapRectangle logicalBound = MapExtensions.NewMapRectangle(
+
+                    west: MapCenter.Longitude - viewWidth / 2,
+                    east: MapCenter.Longitude + viewWidth / 2,
+                    north: MapCenter.Latitude + viewHeight / 2,
+                    south: MapCenter.Latitude - viewHeight / 2
+                );
+
+                Debug.WriteLine("logicalBound " + logicalBound.ToStringUseful());
+
+                AddDescendantRectangles(RootNode, null, logicalBound);
+
+                if (Rectangles.Count > 0)
                 {
-                    West = midLon - viewWidth / 2,
-                    East = midLon + viewWidth / 2,
-                    North = midLat + viewHeight / 2,
-                    South = midLat - viewHeight / 2
-                };
-
-                AddDescendantRectangles(root, null, logicalBound);
+                    Debug.WriteLine("Westernmost rectangle " + Rectangles.Min(r => r.West));
+                    Debug.WriteLine("Easternmost rectangle " + Rectangles.Max(r => r.East));
+                    Debug.WriteLine("Southernmost rectangle " + Rectangles.Min(r => r.South));
+                    Debug.WriteLine("Northernmost rectangle " + Rectangles.Max(r => r.North));
+                }
             }
         }
+
+        enum DescentResult
+        {
+            /// <summary>no intersection of descendants with given region</summary>
+            None = 0,
+            /// <summary>descendants have any points in a given region</summary>
+            PointsCovered = 1,
+            /// <summary>implies PointsCovered and rectangles were added covering descendant points</summary>
+            NewRectangles = 2
+        };
 
         /// <summary>
         /// Here is the principal motivation for Albiruni.
@@ -79,38 +137,47 @@ namespace Abnaki.Albiruni
         /// not descending to any Nodes having Delta < MinimumPrecision.
         /// </summary>
         /// <returns>true if anything added</returns>
-        bool AddDescendantRectangles(Node node, Node parent, MapRectangle logicalBound)
+        DescentResult AddDescendantRectangles(Node node, Node parent, MapRectangle logicalBound)
         {
-            if (node.Delta < MinimumPrecision)
-                return false;
-
             switch (node.Axis)
             {
                 case Axis.NorthSouth:
                     if (node.Degrees > logicalBound.North)
-                        return false;
+                        return DescentResult.None;
                     if (node.Degrees + node.Delta < logicalBound.South)
-                        return false;
+                        return DescentResult.None;
                     break;
                 case Axis.EastWest:
                     if (node.Degrees > logicalBound.East)
-                        return false;
+                        return DescentResult.None;
                     if (node.Degrees + node.Delta < logicalBound.West)
-                        return false;
+                        return DescentResult.None;
                     break;
             }
 
-            bool childRectanglesAdded = false;
+            //if (node.Delta < MinimumPrecision)
+            //{
+            //    if (node.SourceCount > 0)
+            //        return DescentResult.PointsCovered;
+            //    else
+            //        return DescentResult.None;
+            //}
+
+            List<DescentResult> results = new List<DescentResult>();
+            //DescentResult rchild1 = DescentResult.None, rchild2 = DescentResult.None;
             if ( node.Children != null )
             {
-                childRectanglesAdded |= AddDescendantRectangles(node.Children.Item1, node, logicalBound);
-                childRectanglesAdded |= AddDescendantRectangles(node.Children.Item2, node, logicalBound);
+                results.Add(AddDescendantRectangles(node.Children.Item1, node, logicalBound));
+                results.Add(AddDescendantRectangles(node.Children.Item2, node, logicalBound));
             }
 
-            if (childRectanglesAdded)
-                return true;
+            if ( node.SourceCount > 0)
+            {
+                results.Add(DescentResult.PointsCovered);
+            }
 
-            if ( node.Children != null && parent != null && parent.Delta >= MinimumPrecision )
+            if ( results.Any(r => r == DescentResult.PointsCovered) 
+                && parent != null && parent.Delta >= MinimumPrecision )
             {
                 // any descendants exist, and node has not been excluded for view or precision reasons (above)
                 MapRectangle r;
@@ -134,13 +201,15 @@ namespace Abnaki.Albiruni
                 }
                 r.Fill = m_defaultFillBrush;
                 this.Rectangles.Add(r); // efficient ?
-                return true;
+                
+                results.Add(DescentResult.NewRectangles);
             }
-            return false;
+
+            if (results.Count > 0)
+                return (DescentResult)results.Max();
+            else
+                return DescentResult.None;
         }
-
-        System.Windows.Media.Brush m_defaultFillBrush = new SolidColorBrush(Color.FromArgb((byte)32, (byte)255, (byte)0, (byte)0));
-
 
         void ClearAdornments()
         {
