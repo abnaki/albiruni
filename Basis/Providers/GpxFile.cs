dcc350616e212b86d625ad7ddb88490fbcda3a0d
abnaki <abnaki>
2016-06-29 19:20:37 -0400

    Uses IFile rather than public Geo GpsData

--- Basis/Providers/GpxFile.cs
+++ Basis/Providers/GpxFile.cs
@@ -5,9 +5,9 @@ using System.IO;
 using System.Xml;
 using System.Xml.Serialization;
 
-using Geo.Gps;
-using Geo.Gps.Serialization;
-using Geo.Gps.Serialization.Xml;
+//using Geo.Gps;
+//using Geo.Gps.Serialization;
+//using Geo.Gps.Serialization.Xml;
 
 namespace Abnaki.Albiruni.Providers
 {
@@ -15,7 +15,6 @@ namespace Abnaki.Albiruni.Providers
     /// Reads gpx 1.0 or 1.1
     /// </summary>
     /// <remarks>
-    /// Allowing public usage of Geo
     /// </remarks>
     public class GpxFile
     {
@@ -29,49 +28,63 @@ namespace Abnaki.Albiruni.Providers
             if (false == fi.Exists)
                 throw new FileNotFoundException("Nonexistent " + fi.FullName);
 
-            //var gser = NewSerializer(fi);
+            IFile filedat = OpenGpxFile(fi);
+            this.Points = new PointDump(filedat);
 
-            using (Stream str = fi.OpenRead())
-            {
-                GeoGpsData = GpsData.Parse(str);
-
-                //using (StreamWrapper sw = new StreamWrapper(str))
-                //{
-                //    GeoGpsData = gser.DeSerialize(sw);
-                //}
-            }
+            //using (Stream str = fi.OpenRead())
+            //{
+            //    GeoGpsData = GpsData.Parse(str);
+            //}
         }
 
-        public GpsData GeoGpsData { get; private set; }
+        // GpsData is a lowest common denominator 
+        //  with objects in terms of lat,lon and little else.
+        //public GpsData GeoGpsData { get; private set; }
 
-        // unnecessary
-        //IGpsFileSerializer NewSerializer(FileInfo fi)
-        //{
-        //    using (Stream str = fi.OpenRead())
-        //    using (XmlReader xr = XmlTextReader.Create(str) )
-        //    {
-        //        for (int i = 0; i < 5 && xr.Name != "gpx"; i++)
-        //        {
-        //            bool b = xr.Read();
-        //        }
-        //        if (xr.Name != "gpx")
-        //            throw new ApplicationException("Failed to find gpx in " + fi.FullName);
+        public PointDump Points { get; private set; }
 
-        //        string v = xr.GetAttribute("version");
+        IFile OpenGpxFile(FileInfo fi)
+        {
+            string v = null;
+            using (Stream str = fi.OpenRead())
+            using (XmlReader xr = XmlTextReader.Create(str))
+            {
+                for (int i = 0; i < 5 && xr.Name != "gpx"; i++)
+                {
+                    bool b = xr.Read();
+                }
+                if (xr.Name != "gpx")
+                    throw new ApplicationException("Failed to find gpx in " + fi.FullName);
 
-        //        switch ( v )
-        //        {
-        //            case "1.0":
-        //                return new Geo.Gps.Serialization.Gpx10Serializer();
+                v = xr.GetAttribute("version");
+            }
 
-        //            case "1.1":
-        //                return new Geo.Gps.Serialization.Gpx11Serializer();
+            XmlSerializer xser = null;
+            Stream rstr = fi.OpenRead();
+            try {
+                switch (v)
+                {
+                    case "1.0":
+                        xser = new XmlSerializer(typeof(global::Geo.Gps.Serialization.Xml.Gpx.Gpx10.GpxFile));
+                        var gobv10 = (global::Geo.Gps.Serialization.Xml.Gpx.Gpx10.GpxFile)
+                            xser.Deserialize(rstr);
+                        // return GeniusCode.Components.ProxyFactory.DuckInterface<IFile>(gobv10, new Providers.Geo.Gpx.V10.FileDuckProvider());
+                        return Providers.Geo.Gpx.V10.FileDuck.FileFromGpx(gobv10);
 
-        //            default:
-        //                throw new ApplicationException("No support for gpx version " + v + " of " + fi.FullName);
-        //        }
-        //    }
-            
-        //}
+                    case "1.1":
+                        xser = new XmlSerializer(typeof(global::Geo.Gps.Serialization.Xml.Gpx.Gpx11.GpxFile));
+                        var gobv11 = (global::Geo.Gps.Serialization.Xml.Gpx.Gpx11.GpxFile)
+                            xser.Deserialize(rstr);
+                        // return GeniusCode.Components.ProxyFactory.DuckInterface<IFile>(gobv11, new Providers.Geo.Gpx.V11.FileDuckProvider());
+                        return Providers.Geo.Gpx.V11.FileDuck.FileFromGpx(gobv11);
+                }
+            }
+            finally 
+            {
+                rstr.Dispose();
+            }
+
+            throw new ApplicationException("No support for gpx version " + v + " of " + fi.FullName);
+        }
     }
 }
