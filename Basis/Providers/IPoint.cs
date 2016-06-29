933c844fec4b7255108e132d2f321f96e64cd69e
abnaki <abnaki>
2016-06-29 19:21:22 -0400

    Good

---
+++ Basis/Providers/IPoint.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Abnaki.Albiruni.Providers
+{
+    /// <summary>
+    /// Essential commonly recorded data
+    /// </summary>
+    /// <remarks>
+    /// Does not contain nonessential detail that users can explore in a separate 
+    ///  application on the original source file.
+    /// </remarks>
+    public interface IPoint
+    {
+        /// <summary>degrees</summary>
+        decimal Latitude { get; }
+
+        /// <summary>degrees</summary>
+        decimal Longitude { get; }
+
+        /// <summary>meters</summary>
+        decimal? Elevation { get; }
+
+        /// <summary>when visited</summary>
+        DateTime? Time { get; }
+    }
+}
