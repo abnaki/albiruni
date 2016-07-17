876f6c69d49469414e25bb110a7393d71d4e154c
abnaki <abnaki>
2016-07-17 17:49:24 -0400

    +Mesh

---
+++ Basis/Mesh.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Abnaki.Albiruni
+{
+    /// <summary>
+    /// Represents size of a mesh for coordinate system in degrees, adjustable by power of 2.
+    /// </summary>
+    public struct Mesh
+    {
+        public Mesh(int pwr) : this()
+        {
+            power = 0; // temp
+
+            this.Power = pwr;
+        }
+
+        /// <summary>
+        /// Degrees of latitute or longitude
+        /// </summary>
+        public decimal Delta { get; private set; }
+
+        int power;
+
+        /// <summary>
+        /// Mesh Delta is 90 degrees divided by Power of 2.
+        /// </summary>
+        public int Power
+        {
+            get
+            {
+                return power;
+            }
+            set
+            {
+                power = value;
+                Delta = 90 / (decimal)Math.Pow(2d, power);
+            }
+        }
+    }
+}
