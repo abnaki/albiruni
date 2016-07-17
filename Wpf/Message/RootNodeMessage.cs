9184dfa042fb87f5cc1a861ac5e5682c8377b8a2
abnaki <abnaki>
2016-07-17 17:49:25 -0400

    OK

---
+++ Wpf/Message/RootNodeMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Abnaki.Albiruni.Tree;
+
+namespace Abnaki.Albiruni.Message
+{
+    /// <summary>
+    /// Root of all data to view
+    /// </summary>
+    public class RootNodeMessage
+    {
+        public RootNodeMessage(Node root)
+        {
+            this.Root = root;
+        }
+
+        public Node Root { get; private set; }
+    }
+}
