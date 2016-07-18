cea28a2bb4e2f9d4faf40107fe754926d9e2a9b8
abnaki <abnaki>
2016-07-18 13:43:52 -0400

    Refactored into CreateSourceRecords()

--- Wpf/Message/SourceRecordMessage.cs
+++ Wpf/Message/SourceRecordMessage.cs
@@ -13,17 +13,24 @@ namespace Abnaki.Albiruni.Message
     {
         public SourceRecordMessage(IEnumerable<Node> nodes)
         {
-            var groups = from node in nodes
-                                from pair in node.GetSourcesSummaries()
-                                let source = pair.Key
-                                let summary = pair.Value
-                                group summary by source into g select g;
-
-            SourceRecords = (from g in groups
-                            let aggregateSummary = new SourceContentSummary(g)
-                            select new SourceRecord(g.Key, aggregateSummary) ).ToList();
+            this.SourceRecords = CreateSourceRecords(nodes);
         }
 
         public IEnumerable<SourceRecord> SourceRecords { get; private set; }
+
+        public static IEnumerable<SourceRecord> CreateSourceRecords(IEnumerable<Node> nodes)
+        {
+            var groups = from node in nodes
+                         from pair in node.GetSourcesSummaries()
+                         let source = pair.Key
+                         let summary = pair.Value
+                         group summary by source into g
+                         select g;
+
+            return (from g in groups
+                             let aggregateSummary = new SourceContentSummary(g)
+                             select new SourceRecord(g.Key, aggregateSummary)).ToList();
+
+        }
     }
 }
