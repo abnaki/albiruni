79ce38f93b8079d47fac128262dba70a1985efdd
abnaki <abnaki>
2016-07-19 16:01:04 -0400

    MinTime and MaxTime

--- Wpf/SourceRecord.cs
+++ Wpf/SourceRecord.cs
@@ -12,17 +12,20 @@ namespace Abnaki.Albiruni
         {
             this.Source = source;
             this.Summary = summary;
+            this.OverallPoints = summary.FinalSummary();
         }
 
         Source Source { get; set; }
         SourceContentSummary Summary { get; set; }
+        PointSummary OverallPoints { get; set; }
 
         public string Path { get { return this.Source.Path; } }
 
         public int Waypoints { get { return this.Summary.WayPoints.Points;  } }
         public int Trackpoints { get { return this.Summary.TrackPoints.Points; } }
 
-        // want min/max times
+        public DateTime? MinTime { get { return this.OverallPoints.MinTime; } }
+        public DateTime? MaxTime { get { return this.OverallPoints.MaxTime; } }
 
         public override string ToString()
         {
