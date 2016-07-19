66605714ae924fed6b34bb926c4c35ac08ab661d
abnaki <abnaki>
2016-07-19 10:58:44 -0400

    For display of partial source info

---
+++ Wpf/SourceRecord.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Abnaki.Albiruni.Tree;
+
+namespace Abnaki.Albiruni
+{
+    class SourceRecord
+    {
+        public SourceRecord(Source source, SourceContentSummary summary)
+        {
+            this.Source = source;
+            this.Summary = summary;
+        }
+
+        Source Source { get; set; }
+        SourceContentSummary Summary { get; set; }
+
+        public string Path { get { return this.Source.Path; } }
+
+        public int Waypoints { get { return this.Summary.WayPoints.Points;  } }
+        public int Trackpoints { get { return this.Summary.TrackPoints.Points; } }
+
+        // want min/max times
+
+        public override string ToString()
+        {
+            return GetType().Name + ", " + Source + ", " + Summary;
+        }
+    }
+}
