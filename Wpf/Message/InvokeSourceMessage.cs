c88cb4cf04e89e9437834fc7c3a14cdd7d08c9c5
abnaki <abnaki>
2016-07-19 11:08:12 -0400

    OK

---
+++ Wpf/Message/InvokeSourceMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Abnaki.Albiruni.Message
+{
+    /// <summary>
+    /// Externally launch or display a source natively.
+    /// </summary>
+    class InvokeSourceMessage
+    {
+        public InvokeSourceMessage(SourceRecord record)
+        {
+            this.SourceRecord = record;
+        }
+
+        public SourceRecord SourceRecord { get; private set; }
+    }
+}
