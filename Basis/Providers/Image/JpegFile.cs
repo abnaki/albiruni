c79178857e8226c21fa16c817d44dab91636a050
abnaki <abnaki>
2016-07-24 12:09:33 -0400

    Inherits ExifHeader

---
+++ Basis/Providers/Image/JpegFile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+
+namespace Abnaki.Albiruni.Providers.Image
+{
+    class JpegFile : ExifHeader
+    {
+        public const string Extension = ".jpg";
+        // may want to allow alternatives
+
+    }
+}
