0ac8cb52643e0db4ceea122941a190f1ba0db80c
abnaki <abnaki>
2016-07-24 17:59:07 -0400

    +OptionMenuBus

---
+++ Wpf/Menu/OptionMenuBus.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Abnaki.Windows.GUI;
+using Abnaki.Windows.Software.Wpf.Menu;
+using Abnaki.Windows.Software.Wpf.Ultimate;
+
+namespace Abnaki.Albiruni.Menu
+{
+    class OptionMenuBus : ButtonBus<OptionMenuKey>
+    {
+        public OptionMenuBus(IMainMenu menu)
+        {
+            menu.AddCommandChild<OptionMenuKey>(TopMenuKey.Option, OptionMenuKey.Map, "Map");
+            menu.AddCommand(new MenuSeed<OptionMenuKey>()
+            {
+                ParentKey = OptionMenuKey.Map,
+                Key = OptionMenuKey.MapCellColor,
+                Label = "Cell Color"
+            });
+
+            menu.AddExclusiveCommands(OptionMenuKey.MapCellColor, new MenuSeed<OptionMenuKey>[] {
+                new MenuSeed<OptionMenuKey>(OptionMenuKey.MapCellColorRed, "Red", true),
+                new MenuSeed<OptionMenuKey>(OptionMenuKey.MapCellColorGreen, "Green", false),
+                new MenuSeed<OptionMenuKey>(OptionMenuKey.MapCellColorBlue,"Blue", false)
+            });
+
+        }
+
+
+
+    }
+}
---
+++ Wpf/Menu/OptionMenuKey.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace Abnaki.Albiruni.Menu
+{
+    enum OptionMenuKey
+    {
+        Map, // parent
+
+        [Description("Map Cell Color")]
+        MapCellColor,
+
+        MapCellColorRed,
+        MapCellColorGreen,
+        MapCellColorBlue
+    }
+}
