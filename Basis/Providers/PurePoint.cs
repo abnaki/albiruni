36575a70040079b5a1fecb297b600656f2e62365
abnaki <abnaki>
2016-07-24 12:09:33 -0400

    Good

---
+++ Basis/Providers/PurePoint.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Abnaki.Albiruni.Providers
+{
+    /// <summary>
+    /// Only pure Abnaki implementation needed for IPoint
+    /// </summary>
+    public class PurePoint : IPoint
+    {
+        public decimal Latitude
+        {
+            get;
+            set;
+        }
+
+        public decimal Longitude
+        {
+            get;
+            set;
+        }
+
+        public decimal? Elevation
+        {
+            get;
+            set;
+        }
+
+        public DateTime? Time
+        {
+            get;
+            set;
+        }
+    }
+}
