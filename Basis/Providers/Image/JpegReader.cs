c61d4724cf008b31f725005231676b530f2d8356
abnaki <abnaki>
2016-07-24 12:09:33 -0400

    Implements FileReader

---
+++ Basis/Providers/Image/JpegReader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace Abnaki.Albiruni.Providers.Image
+{
+    class JpegReader : FileReader
+    {
+        protected override IFile OpenFile(FileInfo fi)
+        {
+            JpegFile jf = new JpegFile();
+            jf.Read(fi);
+
+            return jf;
+        }
+    }
+}
