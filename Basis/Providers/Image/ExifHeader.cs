e0f9afd62698f9c0be79e86f0245c438687b4975
abnaki <abnaki>
2016-08-11 16:12:29 -0400

    Can read GPSSTimeStamp and GPSDateStamp

--- Basis/Providers/Image/ExifHeader.cs
+++ Basis/Providers/Image/ExifHeader.cs
@@ -22,11 +22,14 @@ namespace Abnaki.Albiruni.Providers.Image
             photo.exif.Parser parser = new photo.exif.Parser();
             IEnumerable<ExifItem> xitems = parser.Parse(fi.FullName).ToList();
 
-            IEnumerable<ExifItem> gpsItems = xitems.Where(x => 1 <= x.Id && x.Id <= 4)
+            IEnumerable<ExifItem> gpsItems = xitems.Where(x => (1 <= x.Id && x.Id <= 7) || x.Id == 29)
                 .OrderByDescending(x => x.Id) // hemispheres will be handled after numbers
                 .ToList();
 
             PurePoint p = new PurePoint();
+
+            TimeSpan? gpsUtcTimeOfDay = null;
+            DateTime? gpsUtcDate = null;
 
             foreach ( ExifItem x in gpsItems )
             {
@@ -47,9 +50,13 @@ namespace Abnaki.Albiruni.Providers.Image
                         p.Longitude = DegreesFromItem(x); 
                         break;
 
-                    case 5: // GPSAltitudeRef i.e. 0 implies positive, 1 implies negative
-                        if (p.Elevation.HasValue && Convert.ToUInt32(x.Value) == 1 )
-                            p.Elevation *= -1;
+                    case 5: // GPSAltitudeRef  byte[1] of which 0 implies positive, 1 implies negative
+                        if (p.Elevation.HasValue)
+                        {
+                            byte[] ba = (byte[])x.Value;
+                            if ( Convert.ToInt32(ba[0]) == 1 )
+                                p.Elevation *= -1;
+                        }
 
                         break;
 
@@ -57,12 +64,43 @@ namespace Abnaki.Albiruni.Providers.Image
                         if (x.Value != null )
                             p.Elevation = DecimalFromURational((URational)x.Value);
                         break;
+
+                    // GPSSTimeStamp  Id=7   expect x.Value to be URational[3] of hours, minutes, seconds, UTC
+                    case 7:
+                        URational[] uar = (URational[])x.Value;
+                        gpsUtcTimeOfDay = TimeSpan.FromHours(DoubleFromURational(uar[0]))
+                             + TimeSpan.FromMinutes(DoubleFromURational(uar[1]));
+                        TimeSpan tsec = TimeSpan.FromSeconds(DoubleFromURational(uar[2]));
+                        if (tsec.TotalMinutes < 1)
+                        {
+                            gpsUtcTimeOfDay += tsec;
+                        }
+                        else
+                        {
+                            // maybe some cameras have the denominator wrong, 
+                            // e.g. iphone 4 has denominator=1 yet it appears it should be 100.
+                            Abnaki.Windows.AbnakiLog.Comment("GPSSTimeStamp has bad seconds " + uar[2], fi.Name);
+                        }
+                        break;
+                   
+                   // GPSDateStamp   Id=29  x.Value is string, believed to be UTC
+                    case 29:
+                        DateTime dtgps;
+                        string sdt = StringFromExif(x.Value);
+                        if (DateTime.TryParseExact(sdt, "yyyy:MM:dd",CultureInfo.InvariantCulture, DateTimeStyles.AdjustToUniversal, out dtgps))
+                        {
+                            gpsUtcDate = dtgps;
+                        }
+
+                        break;
                 }
             }
 
-            // also useful, but not found in sample jpg files:  
-            // GPSSTimeStamp  Id=7   expect x.Value to be URational[3] of hours, minutes, seconds, UTC
-            // GPSDateStamp   Id=29  x.Value is string, believed to be UTC
+            if ( gpsUtcDate.HasValue && gpsUtcTimeOfDay.HasValue )
+            {
+                p.Time = gpsUtcDate.Value + gpsUtcTimeOfDay.Value;
+                p.TimeReliable = true;
+            }
 
             // efforts to salvage other non-GPS data
             if (gpsItems.Any() && false == p.Time.HasValue)
@@ -70,8 +108,7 @@ namespace Abnaki.Albiruni.Providers.Image
                 ExifItem xtime = xitems.FirstOrDefault(x => x.Id == 36867); // DateTimeOriginal
                 if (xtime != null)
                 {
-                    // maybe photo.exif should exclude the ending 0 char:
-                    string sdate = new string(Convert.ToString(xtime.Value).TakeWhile(c => c != 0).ToArray());
+                    string sdate = StringFromExif(xtime.Value);
                     // EXIF 2.3 ignores timezone entirely.
                     // Deduce from location, assuming camera clock is accurate as well.
                     DateTime dt;
@@ -84,7 +121,12 @@ namespace Abnaki.Albiruni.Providers.Image
                         if (DateTime.TryParseExact(sdate, "yyyy:MM:dd HH:mm:ss",
                             CultureInfo.InvariantCulture, DateTimeStyles.AssumeLocal, out dt))
                         {
-                            p.Time = tz.ToUniversalTime(dt);
+                            // lacking GPSDateStamp, this is the last hope for proper UTC date
+                            DateTime dtApproxUtc = tz.ToUniversalTime(dt);
+                            if (gpsUtcTimeOfDay.HasValue)
+                                p.Time = dtApproxUtc.Date + gpsUtcTimeOfDay.Value; // supposed to be best
+                            else
+                                p.Time = dtApproxUtc;
                         }
 
                     }
@@ -92,6 +134,12 @@ namespace Abnaki.Albiruni.Providers.Image
             }
 
             singlePoint = p;
+        }
+
+        static string StringFromExif(object v)
+        {
+            // maybe photo.exif should exclude the ending 0 char:
+            return new string(Convert.ToString(v).TakeWhile(c => c != 0).ToArray());
         }
 
         static decimal DegreesFromItem(ExifItem x)
@@ -102,6 +150,11 @@ namespace Abnaki.Albiruni.Providers.Image
         static decimal DecimalFromURational(URational r, int furtherDivideBy = 1)
         {
             return decimal.Divide((decimal)r.Numerator, (decimal)r.Denominator * (decimal)furtherDivideBy);
+        }
+
+        static double DoubleFromURational(URational r, int furtherDivideBy = 1)
+        {
+            return (double)DecimalFromURational(r, 1);
         }
 
         static decimal DegreesFromArray(URational[] dms)
