7bc325bb025f387dc2372c563f0d7999f8e2f7fd
abnaki <abnaki>
2016-08-12 12:55:14 -0400

    Allows for TSCLIENT

--- Wpf/sign.bat
+++ Wpf/sign.bat
@@ -1,3 +1,7 @@
 @echo off
+set KDRIVE=K:
+if not exist %1 echo Nonexistent %1
+if not exist %KDRIVE% set KDRIVE=\\TSCLIENT\K
+if not exist %KDRIVE% echo %KDRIVE% not found.
 set /p pw="Passphrase: "
-"c:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\signtool.exe" sign /f k:\cert\godaddy\abnaki.pfx /p %pw%  /tr http://tsa.starfieldtech.com /td SHA256 %1
+"c:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\signtool.exe" sign /f %KDRIVE%\cert\godaddy\abnaki.pfx /p %pw%  /tr http://tsa.starfieldtech.com /td SHA256 %1
