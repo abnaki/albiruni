65d653cdc59eadb5b37c850217f3c1d9e105e83a
abnaki <abnaki>
2016-08-15 12:20:09 -0400

    +PointReader

---
+++ Basis/Providers/PointReader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Abnaki.Albiruni.Providers
+{
+    /// <summary>
+    /// Base class that can read or concoct a PointDump
+    /// </summary>
+    public class PointReader
+    {
+        public PointDump Points { get; protected set; }
+
+    }
+}
