cded8538c7b37b5403a7fdd1f8622875080c09a5
abnaki <abnaki>
2016-08-19 10:31:27 -0400

    OpenStreetMapParallel (max downloads)

--- Wpf/Properties/Settings.Designer.cs
+++ Wpf/Properties/Settings.Designer.cs
@@ -34,5 +34,17 @@ namespace Abnaki.Albiruni.Properties {
                 this["OpenStreetMapTileUrl"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("2")]
+        public int OpenStreetMapParallel {
+            get {
+                return ((int)(this["OpenStreetMapParallel"]));
+            }
+            set {
+                this["OpenStreetMapParallel"] = value;
+            }
+        }
     }
 }
