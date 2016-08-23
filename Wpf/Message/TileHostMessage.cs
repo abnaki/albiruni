cb1d75362e9526a1b3e852339d2f13da33e4a72d
abnaki <abnaki>
2016-08-23 14:40:41 -0400

    OK

---
+++ Wpf/Message/TileHostMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Abnaki.Albiruni.TileHost;
+
+namespace Abnaki.Albiruni.Message
+{
+    class TileHostMessage
+    {
+        public TileHostMessage(LocatorTemplate loctemp)
+        {
+            this.LocatorTemplate = loctemp;
+        }
+
+        public LocatorTemplate LocatorTemplate { get; private set; }
+    }
+}
