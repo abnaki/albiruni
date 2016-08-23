7c3d8a79472b719de6d6dfdc74f7431866da658e
abnaki <abnaki>
2016-08-23 14:40:42 -0400

    OK

---
+++ Wpf/TileHost/TiRecord.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using PropertyChanged;
+
+namespace Abnaki.Albiruni.TileHost
+{
+    /// <summary>
+    /// View of some properties for UI
+    /// </summary>
+    [ImplementPropertyChanged]
+    class TiRecord
+    {
+        public TiRecord(LocatorTemplate loctemp)
+        {
+            this.LocatorTemplate = loctemp;
+        }
+
+        public LocatorTemplate  LocatorTemplate { get; private set; }
+
+        public string Host { get { return this.LocatorTemplate.Org.Domain.Host;  } }
+
+        public string Style { get { return this.LocatorTemplate.Subdirectory;  } }
+
+        public bool Select { get; set; }
+
+        // want a tested sample image
+    }
+}
