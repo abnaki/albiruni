bb8a314552289d2387034042c8a13908914f10c9
abnaki <abnaki>
2016-08-27 16:58:45 -0400

    +PartialKey

--- Wpf/TileHost/TiRecord.cs
+++ Wpf/TileHost/TiRecord.cs
@@ -19,11 +19,23 @@ namespace Abnaki.Albiruni.TileHost
 
         public LocatorTemplate  LocatorTemplate { get; private set; }
 
-        public string Host { get { return this.LocatorTemplate.Org.Domain.Host;  } }
+        public string Host { get { return this.LocatorTemplate.Org.Domain.Uri.Host;  } }
 
         public string Style { get { return LocatorTemplate.Style ?? LocatorTemplate.Subdirectory;  } }
 
         public bool Select { get; set; }
+
+        public string PartialKey
+        {
+            get
+            {
+                string key = this.LocatorTemplate.Org.UserKey;
+                if (string.IsNullOrEmpty(key))
+                    return null;
+
+                return string.Join("",key.Take(12)) + "..."; // confidential
+            }
+        }
 
         // want a tested sample image
     }
