4fa528d3311207f08fe7384f2f2d5a49c8af5baa
abnaki <abnaki>
2016-08-27 16:58:44 -0400

    OK

---
+++ Wpf/TileHost/TileHostSupply.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+using Abnaki.Windows.Xml;
+using Abnaki.Windows;
+using Abnaki.Windows.Software.Wpf.Diplomat;
+
+namespace Abnaki.Albiruni.TileHost
+{
+    /// <summary>
+    /// Allows limited external configurations of objects
+    /// </summary>
+    public class TileHostSupply
+    {
+        public TileHostSupply()
+        {
+            Organizations = new List<Organization>();
+        }
+
+        public List<Organization> Organizations { get; private set; }
+
+        public const string ConfigFilename = "MapTileHosts.xml";
+
+        public void Write()
+        {
+            FileInfo fi = XmlFileInfo();
+            AbnakiXml.Write(fi.FullName, this, Subtypes());
+        }
+
+        public static TileHostSupply Read()
+        {
+            try
+            {
+                FileInfo fi = XmlFileInfo();
+                if (fi.Exists)
+                    return AbnakiXml.Read<TileHostSupply>(fi, Subtypes());
+            }
+            catch ( Exception ex )
+            {
+                Notifier.Error(ex);
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Existing static objects are set substantially equal to values inside this class
+        /// </summary>
+        public void SquareUpStatics()
+        {
+            foreach ( Organization org in Organization.CommercialProviders() )
+            {
+                Organization userDefinedOrg = FindOrganization(org);
+                if (userDefinedOrg != null)
+                    org.SquareUp(userDefinedOrg);
+            }
+        }
+
+        Organization FindOrganization(Organization basis)
+        {
+            return this.Organizations.Where(org => org.FileKey != null).FirstOrDefault(org => org.FileKey == basis.FileKey);
+        }
+
+        static FileInfo XmlFileInfo()
+        {
+            var a = System.Reflection.Assembly.GetEntryAssembly();
+            string dir;
+            if (a == null)
+                dir = Environment.CurrentDirectory;
+            else
+                dir = Path.GetDirectoryName(a.Location);
+
+            return new FileInfo(Path.Combine(dir, ConfigFilename));
+        }
+
+        static Type[] Subtypes()
+        {
+            return new Type[] {  typeof(Organization) };
+        }
+
+    }
+}
