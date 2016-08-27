3183107eae4cc2b4db18713a7c2924dc534718d9
abnaki <abnaki>
2016-08-27 16:58:43 -0400

    +TileHostTest

---
+++ Tests/MapTest1/TileHostTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+using System.Diagnostics;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Abnaki.Albiruni.TileHost;
+using Abnaki.Windows.Xml;
+
+namespace MapTest1
+{
+    [TestClass]
+    public class TileHostTest
+    {
+        [TestMethod]
+        public void TestTileHostSupply()
+        {
+            try
+            {
+                TileHostSupply supply = new TileHostSupply();
+                supply.Organizations.Add(Organization.Mapbox);
+
+                supply.Write();
+
+                TileHostSupply supplyReturned = TileHostSupply.Read();
+
+                //Debug.WriteLine("Write/read returned " + orgReturned);
+                //Organization.Mapbox.SquareUp(orgReturned);
+
+                supplyReturned.SquareUpStatics();
+            }
+            catch ( Exception ex )
+            {
+                throw;
+            }
+        }
+    }
+}
