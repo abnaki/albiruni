c1c6e5f176a36335657ba21eade0ac199fcc5d3e
abnaki <abnaki>
2016-08-29 14:01:02 -0400

    Prompt text

--- Build/sign.bat
+++ Build/sign.bat
@@ -2,6 +2,6 @@
 set KDRIVE=K:
 if not exist %KDRIVE% set KDRIVE=\\TSCLIENT\K
 if not exist %KDRIVE% echo %KDRIVE% not found.
-set /p pw="Passphrase: "
+set /p pw="Passphrase to signtool: "
 
 for %%F in ( %* ) do "c:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\signtool.exe" sign /f %KDRIVE%\cert\godaddy\abnaki.pfx /p %pw%  /tr http://tsa.starfieldtech.com /td SHA256 %%F
