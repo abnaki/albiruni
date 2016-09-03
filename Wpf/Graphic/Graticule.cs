81d0185f5d89eb2acd858636eb7f84711569aed3
abnaki <abnaki>
2016-09-03 17:34:28 -0400

    +Graticule

---
+++ Wpf/Graphic/Graticule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Abnaki.Albiruni.Graphic
+{
+    class Graticule : MapControl.MapGraticule
+    {
+        protected override void OnRender(System.Windows.Media.DrawingContext drawingContext)
+        {
+            if (IsEnabled)
+            {
+                base.OnRender(drawingContext);
+            }
+        }
+    }
+}
