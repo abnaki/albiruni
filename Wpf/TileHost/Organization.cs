d411f46739dfc2c721a68a6848ece9a3db71d4eb
abnaki <abnaki>
2016-09-14 13:47:22 -0400

    Separated Organization.Statics.cs

--- Wpf/TileHost/Organization.cs
+++ Wpf/TileHost/Organization.cs
@@ -9,7 +9,7 @@ namespace Abnaki.Albiruni.TileHost
     /// <summary>
     /// Independent legal entity that serves up map tiles
     /// </summary>
-    public class Organization : IComparable<Organization>
+    public partial class Organization : IComparable<Organization>
     {
         public Organization(string partUri, IEnumerable subdoms = null)
         {
@@ -95,70 +95,6 @@ namespace Abnaki.Albiruni.TileHost
             return this.Domain.CompareTo(other.Domain);
         }
 
-        const string citeosm = "[OpenStreetMap Contributors](http://openstreetmap.org/copyright)";
-
-        public const string UndefinedKey = "undefined";
-
-        public static readonly Organization Carto = new Organization("http://basemaps.cartocdn.com", "abcd")
-        {
-            Copyright = "[CARTO](https://carto.com/attributions), CC BY 3.0, Data by " + citeosm + ", ODbL"
-        };
-
-        public static readonly Organization Stamen = new Organization("http://tile.stamen.com")
-        {
-            Copyright = "[Stamen Design](http://stamen.com/), Data by " + citeosm
-        };
-
-        public static readonly Organization Osm = new Organization("http://tile.openstreetmap.org", "abc")
-        {
-            Copyright = citeosm
-        };
-
-        public static readonly Organization MapXyz = new Organization("http://osm.maptiles.xyz", "abc")
-        {
-            Copyright = citeosm
-        };
-
-        public static readonly Organization WmfLabs = new Organization("http://tiles.wmflabs.org", "abc")
-        {
-            Copyright = "[wmflabs](http://wmflabs.org/), Data by " + citeosm
-        };
-
-        public static readonly Organization Thunderforest = new Organization("http://tile.thunderforest.com", "abc")
-        {
-            Copyright = citeosm
-        };
-
-        public static readonly Organization Mapbox = new Organization("https://tiles.mapbox.com", "abcd")
-        {
-            Copyright = "[Mapbox](https://www.mapbox.com/about/maps/), " + citeosm,
-            FileKey = "mapbox",
-            AllowMultiUserCache = false, // terms of service
-            UserKey = UndefinedKey,
-            UriDelimitUserKey = "?access_token="
-        };
-
-        public static readonly Organization Here = new Organization("https://maps.cit.api.here.com","1234")
-        {
-            Copyright = "[HERE](http://developer.here.com)",
-            FileKey = "here.com",
-            AllowMultiUserCache = false, // terms of service
-            UserKey = UndefinedKey,
-            UriDelimitUserKey = "?" // requires 2 explicit parameters
-        };
-
-        public static IEnumerable<Organization> CommercialProviders()
-        {
-            yield return Mapbox;
-            yield return Here;
-        }
-
-        //public static readonly Organization OpenPublicTransport = new Organization("http://www.openptmap.org")
-        //{
-        //    Copyright = citeosm
-        //};
-
-        // public static readonly Organization Usgs = new Organization("http://basemap.nationalmap.gov");
 
 
     }
