a76339e9d407228ad4f27fc83911a048fef5e746
abnaki <abnaki>
2016-09-17 12:17:14 -0400

    OK

---
+++ Wpf/Message/DrawSourceMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Abnaki.Albiruni.Message
+{
+    /// <summary>
+    /// Issued to draw Source graphically in detail
+    /// </summary>
+    class DrawSourceMessage
+    {
+        public DrawSourceMessage(SourceRecord r)
+        {
+            this.SourceRecord = r;
+        }
+
+        public SourceRecord SourceRecord { get; private set; }
+    }
+}
