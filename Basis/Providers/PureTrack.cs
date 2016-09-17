8ba7330148dc68f4e941dddf1d8d8706d2f32237
abnaki <abnaki>
2016-09-17 12:17:11 -0400

    ITrack

---
+++ Basis/Providers/PureTrack.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Abnaki.Albiruni.Providers
+{
+    class PureTrack : ITrack
+    {
+        public IEnumerable<IPoint> Points
+        {
+            get;
+            set;
+        }
+    }
+}
