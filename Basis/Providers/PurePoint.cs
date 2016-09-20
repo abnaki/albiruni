70df324534f3428c63203ad1b9eb01574f738439
abnaki <abnaki>
2016-09-20 12:32:18 -0400

    +Write(), Read(), PurePoint(IPoint)

--- Basis/Providers/PurePoint.cs
+++ Basis/Providers/PurePoint.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
+using Abnaki.Windows;
 
 namespace Abnaki.Albiruni.Providers
 {
@@ -12,6 +14,15 @@ namespace Abnaki.Albiruni.Providers
         public PurePoint()
         {
 
+        }
+
+        public PurePoint(IPoint p)
+        {
+            this.Latitude = p.Latitude;
+            this.Longitude = p.Longitude;
+            this.Elevation = p.Elevation;
+            this.Time = p.Time;
+            this.TimeReliable = p.TimeReliable;
         }
 
         public PurePoint(decimal lat, decimal lon)
@@ -48,6 +59,28 @@ namespace Abnaki.Albiruni.Providers
         {
             get;
             set;
+        }
+
+        const int version = 1;
+
+        public void Write(BinaryWriter bw)
+        {
+            bw.Write(version);
+            bw.Write(Latitude);
+            bw.Write(Longitude);
+            AbnakiFile.WriteNullable(Time, bw);
+            bw.Write(TimeReliable);
+            AbnakiFile.WriteNullable(Elevation, bw);
+        }
+
+        public void Read(BinaryReader br)
+        {
+            int readVersion = br.ReadInt32();
+            Latitude = br.ReadDecimal();
+            Longitude = br.ReadDecimal();
+            Time = AbnakiFile.ReadNullableDateTime(br);
+            TimeReliable = br.ReadBoolean();
+            Elevation = AbnakiFile.ReadNullableDecimal(br);
         }
 
         public override string ToString()
