dba7833257cb94dcee07726094afbf4ba4f20a18
abnaki <abnaki>
2016-09-22 11:03:49 -0400

    +TileLoaderMessage

---
+++ Wpf/Message/TileLoaderMessage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Abnaki.Albiruni.TileHost;
+
+namespace Abnaki.Albiruni.Message
+{
+    /// <summary>
+    /// Logically occurs after TileHostMessage was handled
+    /// and then TileImageLoader was updated according to a LocatorTemplate.
+    /// </summary>
+    class TileLoaderMessage
+    {
+        public TileLoaderMessage(LocatorTemplate loctemp)
+        {
+            this.LocatorTemplate = loctemp;
+        }
+
+        public LocatorTemplate LocatorTemplate { get; private set; }
+
+    }
+}
