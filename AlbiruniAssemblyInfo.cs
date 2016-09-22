fd1985f260080a9e174ec7c89a86621f7629c57f
abnaki <abnaki>
2016-09-22 11:25:05 -0400

    1.0.11.0

--- AlbiruniAssemblyInfo.cs
+++ AlbiruniAssemblyInfo.cs
@@ -1,7 +1,7 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.9.0")]
-[assembly: AssemblyFileVersion("1.0.9.0")]
+[assembly: AssemblyVersion("1.0.11.0")]
+[assembly: AssemblyFileVersion("1.0.11.0")]
 
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Abnaki Light Industry")]
